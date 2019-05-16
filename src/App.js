bf0a6dc870fbe0ce7f343a31e18f1d090164e5c5
Jerry Green <jerrygreenest@gmail.com>
2019-05-16 15:11:58 +0500

    changes to reproduce issue 13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useResource } from 'react-request-hook'
 
 function App() {
+  const test = useResource(() => ({}))
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RequestProvider } from 'react-request-hook';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RequestProvider value={axios}>
+    <App/>
+  </RequestProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
